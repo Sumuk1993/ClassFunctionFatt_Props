f125980def78ff089a64334c14aba5f8fdb49af9
EXCELINDIA\sumukh.putahnkar <sumukh.sathyanarayanaputhankar1@pearson.com>
2025-01-27 16:53:42 +0530

    file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ClassProps from './components/ClassProps';
 function App() {
   return (
     <div>Good Morning <ClassProps surname="Kashyap" >
-
+      
       </ClassProps>
     </div>
   );
